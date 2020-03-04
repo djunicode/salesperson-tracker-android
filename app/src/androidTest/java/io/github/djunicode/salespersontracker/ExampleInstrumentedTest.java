17853e15f2e96fecfb436fb144d332749a0fc6ed
MihirS57 <shahmihir507@gmail.com>
2020-03-04 23:33:14 +0530

    Login Page Done

--- app/src/androidTest/java/io/github/djunicode/salespersontracker/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/io/github/djunicode/salespersontracker/ExampleInstrumentedTest.java
@@ -22,6 +22,6 @@ public class ExampleInstrumentedTest {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
 
-        assertEquals("io.github.djunicode.salespersontracker", appContext.getPackageName());
+        assertEquals( "io.github.djunicode.salespersontracker", appContext.getPackageName() );
     }
 }
