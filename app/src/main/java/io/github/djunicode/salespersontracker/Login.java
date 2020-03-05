85a6075429a3534500231934fae4671134ad691a
MihirS57 <shahmihir507@gmail.com>
2020-03-05 23:59:58 +0530

    Login Page is Ready

---
+++ app/src/main/java/io/github/djunicode/salespersontracker/Login.java
@@ -0,0 +1,34 @@
+package io.github.djunicode.salespersontracker;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class Login extends AppCompatActivity {
+
+    TextView signup_link;
+    EditText email;
+    EditText pass;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate( savedInstanceState );
+        setContentView( R.layout.login );
+
+        email = findViewById( R.id.editEmail );
+        pass = findViewById( R.id.editPass );
+        signup_link = findViewById( R.id.link_signup );
+        signup_link.setOnClickListener( new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Login.this,Signup.class);
+
+                startActivity( intent );
+            }
+        } );
+
+    }
+}
