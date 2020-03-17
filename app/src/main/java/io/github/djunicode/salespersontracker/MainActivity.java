aae95eacc5d01a00358711890cb6586fbcf0bba9
BhaviDave <bhavidave5@gmail.com>
2020-03-18 01:23:27 +0530

    UI Merge Conflicts

--- app/src/main/java/io/github/djunicode/salespersontracker/MainActivity.java
+++ app/src/main/java/io/github/djunicode/salespersontracker/MainActivity.java
@@ -11,11 +11,10 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Context;
 import android.os.Bundle;
-<<<<<<< HEAD
+
 import android.view.MenuItem;
 import android.widget.Toast;
-||||||| 04aa887
-=======
+
 import android.view.MenuItem;
 import android.view.animation.AnimationUtils;
 
@@ -25,21 +24,16 @@ import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
->>>>>>> 34ad03e00ea5587c5ca20884f9643d718fabb493
 
-<<<<<<< HEAD
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentTransaction;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
-||||||| 04aa887
-import androidx.appcompat.app.AppCompatActivity;
-=======
->>>>>>> 34ad03e00ea5587c5ca20884f9643d718fabb493
 
-<<<<<<< HEAD
+import androidx.appcompat.app.AppCompatActivity;
+
 public class MainActivity extends AppCompatActivity {
     BottomNavigationView mBottomNavigationView;
     LocationFragment mLocationFragment;
@@ -69,15 +63,12 @@ public class MainActivity extends AppCompatActivity {
         }
 
     };
-||||||| 04aa887
-public class MainActivity extends AppCompatActivity {
-=======
->>>>>>> 34ad03e00ea5587c5ca20884f9643d718fabb493
-
-public class MainActivity extends AppCompatActivity  {
 
 
-    BottomNavigationView mBottomNavigationView;//Fragment active;
+
+
+
+   // BottomNavigationView mBottomNavigationView;//Fragment active;
 
 String s1[]={"Dummy title 1","Dummy title 2","Dummy title 3","Dummy title 4","Dummy title 5" };
     String s2[]={"Dummy content 1","Dummy content 2","Dummy content 3","Dummy content 4","Dummy content 5" };
@@ -85,7 +76,7 @@ String s1[]={"Dummy title 1","Dummy title 2","Dummy title 3","Dummy title 4","Du
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-<<<<<<< HEAD
+
 
         mBottomNavigationView=findViewById(R.id.bottom_navigation);
         mLocationFragment=new LocationFragment();
@@ -179,64 +170,5 @@ String s1[]={"Dummy title 1","Dummy title 2","Dummy title 3","Dummy title 4","Du
         ft.commit();
     }
 
-   /*
-    void loadFragment(Fragment fragment) {
-        //switching fragment
-        if (fragment != null) {
-            getSupportFragmentManager()
-                    .beginTransaction()
-                    .replace(R.id.frame_layout_main, fragment)
-                    .commit();
 
-        }
-      else {
-            Toast.makeText(this,"NULL FRAG",Toast.LENGTH_LONG).toString();
-        }
-    }
-*/
 }// main activity ends
-||||||| 04aa887
-    }
-}
-=======
-       
-
-        mBottomNavigationView=findViewById(R.id.bottom_navigation);
-
-        mBottomNavigationView.setOnNavigationItemReselectedListener(
-                new BottomNavigationView.OnNavigationItemReselectedListener() {
-                    @Override
-                    public void onNavigationItemReselected(@NonNull MenuItem item) {
-                      //  Fragment fragment;
-                        switch (item.getItemId()) {
-                            case R.id.Location:
-                                Fragment location = new LocationFragment();
-                                FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
-                                transaction.replace(R.id.frame_layout_main, location);
-                                transaction.addToBackStack(null);
-                                transaction.commit();
-                            case R.id.Checkpoints:
-                                Fragment check = new CheckpointsFragment();
-                                FragmentTransaction transaction1 = getSupportFragmentManager().beginTransaction();
-                                transaction1.replace(R.id.frame_layout_main, check);
-                                transaction1.addToBackStack(null);
-                                transaction1.commit();
-                            case R.id.UpdateTargets:
-                                Fragment update = new CheckpointsFragment();
-                                FragmentTransaction transaction2 = getSupportFragmentManager().beginTransaction();
-                                transaction2.replace(R.id.frame_layout_main, update);
-                                transaction2.addToBackStack(null);
-                                transaction2.commit();
-                        }
-                    }
-                });
-
-
-
-       
-
-
-
-
-}//mainactivity ends here
->>>>>>> 34ad03e00ea5587c5ca20884f9643d718fabb493
--- app/src/main/java/io/github/djunicode/salespersontracker/UpdateTargetFragment.java
+++ app/src/main/java/io/github/djunicode/salespersontracker/UpdateTargetFragment.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package io.github.djunicode.salespersontracker;
 
 import android.os.Bundle;
@@ -40,49 +39,3 @@ public class UpdateTargetFragment extends Fragment {
         return r;
     }
 }
-||||||| 04aa887
-=======
-package io.github.djunicode.salespersontracker;
-
-
-import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
-
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-
-
-/**
- * A simple {@link Fragment} subclass.
- */
-public class UpdateTargetFragment extends Fragment {
-
-
-    public UpdateTargetFragment() {
-        // Required empty public constructor
-    }
-
-    String s1[]={"Dummy title 1","Dummy title 2","Dummy title 3","Dummy title 4","Dummy title 5","Dummy title 6","Dummy title 7","Dummy title 8","Dummy title 9","Dummy title 10" };
-    String s2[]={"Dummy content 1","Dummy content 2","Dummy content 3","Dummy content 4","Dummy content 5","Dummy content 6" ,"Dummy content 7","Dummy content 8","Dummy content 9","Dummy content 10"};
-
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-            Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-         View r=inflater.inflate(R.layout.fragment_update_target, container, false);
-        RecyclerView r2 = r.findViewById(R.id.UpdateTargetsRecyclerView);
-        RecyclerView.Adapter adapter2 = new UpdateTargetAdapter(getContext(), s1, s2);
-
-        r2.setAdapter(adapter2);
-        r2.setLayoutManager(new LinearLayoutManager(getContext()));
-
-
-        return r;
-    }
-
-}
->>>>>>> 34ad03e00ea5587c5ca20884f9643d718fabb493
